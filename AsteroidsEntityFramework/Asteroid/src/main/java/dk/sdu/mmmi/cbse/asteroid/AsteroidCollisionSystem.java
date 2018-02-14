3e301dc8829ce3b4a1e0a627263128f0a1079218
Agger1995 <christian_agger1995@hotmail.com>
2018-02-14 16:57:46 +0100

    Begun implementing the collision system. I think the Enemy and Player must have a LifePart in them.

---
+++ AsteroidsEntityFramework/Asteroid/src/main/java/dk/sdu/mmmi/cbse/asteroid/AsteroidCollisionSystem.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.asteroid;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+
+/**
+ *
+ * @author Agger
+ */
+public class AsteroidCollisionSystem implements IPostEntityProcessingService {
+
+    /**
+     * Asteroid should be split up into 2 smaller Asteroids if hit by EnemyProjectile or PlayerProjectile.
+     * @param gameData
+     * @param world 
+     */
+    @Override
+    public void process(GameData gameData, World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ AsteroidsEntityFramework/Enemy/src/main/java/dk/sdu/mmmi/cbse/enemy/EnemyCollisionSystem.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.enemy;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
+
+/**
+ *
+ * @author Agger
+ */
+public class EnemyCollisionSystem implements IPostEntityProcessingService {
+
+    /**
+     * Enemy ship should be destroyed if hit by an Asteroid or by a PlayerProjectile twice
+     * @param gameData
+     * @param world 
+     */
+    @Override
+    public void process(GameData gameData, World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
